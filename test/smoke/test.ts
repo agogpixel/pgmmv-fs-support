a2061467943cdaa356de12c01967077fb8155cc8
kidthales <kidthales@agogpixel.com>
2022-05-21 09:16:07 -0700

    Fix smoke test.

--- test/smoke/test.ts
+++ test/smoke/test.ts
@@ -1,4 +1,4 @@
-import { createFileSystemModule } from '@agogpixel/pgmmv-fs-support';
+import { createFileSystemModule } from '@agogpixel/pgmmv-fs-support/src/file-system-module';
 
 describe('createFileSystemModule', () => {
   it('is a function', () => expect(typeof createFileSystemModule).toBe('function'));
